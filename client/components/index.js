d544dbe597c71b980d4fddb142078b05744d163f
njasi <nick@jasinski3.com>
2019-11-05 13:33:07 -0600

    updated components index.jks

--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,5 @@
+
+
 /**
  * `components/index.js` exists simply as a 'central export' for our components.
  * This way, we can import all of our components from the same place, rather than
@@ -5,4 +7,5 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as SingleProject} from './SingleProject'
 export {Login, Signup} from './auth-form'
