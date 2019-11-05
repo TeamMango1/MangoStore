6a6ca11d6f97bbc43961b40e6b4cb16c447ae2ab
njasi <nick@jasinski3.com>
2019-11-05 13:28:15 -0600

    Base singleProject setup complete

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+export const SingleProduct = props => {
+  return (
+    <div>
+      <img src={props.photoUrl} />
+      <h1>{props.name}</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Price</td>
+            <td>{props.price}</td>
+          </tr>
+          <tr>
+            <td>Stock</td>
+            <td>{props.inventory}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>{props.description}</p>
+      <button onClick={props.addToCart}>Add to cart</button>
+    </div>
+  )
+}
+
+const mapState = state => {
+  return {
+    ...state.singleProject
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    addToCart: () => dispatch({type: 'TEMP'})
+  }
+}
+
+export default connect(
+  mapState,
+  mapDispatch
+)(SingleProduct)
+
+/**
+ * PROP TYPES
+ */
+Navbar.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+}
