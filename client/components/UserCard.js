5decfc476e46bad2fa58355f4bbe7aec7bf990f7
Jian Cong Ma <jiancongma@protonmail.com>
2019-11-06 13:54:32 -0600

    split name into first and last and change userId to id to fectch data

--- client/components/UserCard.js
+++ client/components/UserCard.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
 const UserCard = props => {
+  const user = props.user
   return (
     <div>
-      <div>
-        {props.user.firstName} {props.user.lastName}
-      </div>
-      <div>{props.user.email}</div>
+      <Link to={`/users/${user.id}`}>
+        <div>
+          {user.firstName} {props.user.lastName}
+        </div>
+        <div>{user.email}</div>
+      </Link>
     </div>
   )
 }
