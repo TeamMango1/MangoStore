7804198ecff8637dbcc1b52dce19c0614a2f1095
njasi <nick@jasinski3.com>
2019-11-07 12:26:04 -0600

    added Mango emoji

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,7 +14,7 @@ class Navbar extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <h1 className="navbar-brand">Mangonificent™</h1>
+          <h1 className="navbar-brand">Mangonificent™ 🥭</h1>
           <div className="navbar-nav">
             {this.props.isLoggedIn ? (
               <React.Fragment>
