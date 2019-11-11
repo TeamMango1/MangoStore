73da03ba17341013bf2adf47f8dc1233a61eb0cb
njasi <nick@jasinski3.com>
2019-11-11 14:00:42 -0600

    ypdate order model

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import StripeCheckout from 'react-stripe-checkout'
 import CartItem from './CartItem'
 import {removeFromCart, fetchCart} from '../store/cartReducer'
 
 class Cart extends React.Component {
+  constructor() {
+    super()
+    this.handleToken = this.handleToken.bind(this)
+  }
   componentDidMount() {
     this.props.getCart()
   }
+
+  handleToken(token, addresses) { // paid for
+    console.log(token, addresses)
+
+
+
+  }
+
   render() {
+    let cartTotal = 0
     const cart = this.props.cart ? this.props.cart : []
     return (
       <div className="container">
         <div className="row">
           {cart.map(item => {
+            cartTotal += item.price
             return (
               <CartItem
                 key={item.id}
@@ -21,6 +36,17 @@ class Cart extends React.Component {
               />
             )
           })}
+        </div>
+        <br/>
+        <div className = "d-flex justify-content-center">
+          <StripeCheckout
+            stripeKey="pk_test_kZmJSR4cNZc7FGBq3pfyRkaH00UmOKDepu"
+            token={this.handleToken}
+            amount={cartTotal}
+            name="Your Cart"
+            billingAddress
+            shippingAddress
+          />
         </div>
       </div>
     )
