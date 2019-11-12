e13eedbe6047e56e27556b1ad91c4fe852c1fad4
Munaiz <munaizahmed2@gmail.com>
2019-11-12 16:08:03 -0600

    writing test specs for seed file

--- script/seed.spec.js
+++ script/seed.spec.js
@@ -1,6 +1,11 @@
 'use strict'
 /* global describe it */
 
+const { expect } = require('chai');
+const { db } = require('../server/db')
+const seed = require('./seed')
+const { Product, User, Category, Review, } = require('../../server/db/models')
+
 const seed = require('./seed')
 
 describe('seed script', () => {
